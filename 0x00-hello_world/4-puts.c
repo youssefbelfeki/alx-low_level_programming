c95d17e9686a3a4a0b3608d4d090f77013685343
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-08 00:53:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include <stdio.h>/
 /**
  *
  *  * Description: main - Prints out a string to stdout.
@@ -7,13 +6,8 @@
  *   * Return: 0 if success.
  *
  *    */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
