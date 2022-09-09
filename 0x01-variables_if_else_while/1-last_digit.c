c9a7a7c005e0dfd4929efaa0c2d155a0226c5758
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-09 15:48:14 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", 
+printf("Last digit of %d is %d and is less than 6 and not 0\n", 	
 n, n % 10);
 }
 else
