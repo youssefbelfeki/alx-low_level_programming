3320fca82eedfe1cd03009115904bac0e912b5af
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-09 15:53:49 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,7 @@ int main(void)
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-if (c != 'e' && c != 'q')							{
-putchar(c);
-}
+if (c != 'e' && c != 'q')							putchar(c);
 }
 putchar('\n');
 return (0);
