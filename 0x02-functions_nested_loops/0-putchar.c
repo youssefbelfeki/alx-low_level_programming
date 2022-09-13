39c33a337b05e36374501d7653e14c63548be14d
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-13 14:41:49 -0700

    vi 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * Return: always 0
  */
 int main(void)
-{
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -13,6 +13,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n');					
+_putchar('\n');
 return (0);
 }
