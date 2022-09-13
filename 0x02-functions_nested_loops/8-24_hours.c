58f475328c0f7a24f62ab85c52032838d2ddb0e4
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-13 15:58:47 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,10 @@ for (minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
-_putchar(':');									_putchar((minute / 10) + '0');							_putchar((minute % 10) + '0');							_putchar('\n');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
 }
 }
 }
