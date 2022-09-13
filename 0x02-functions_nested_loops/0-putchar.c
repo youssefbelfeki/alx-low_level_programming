fe66e122db833fd0dae6992fd39e769a632ed1c4
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-13 14:49:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/**
- * main - print putchar
- * Return: always 0
- */
+/*** main - print putchar
+* Return: always 0
+*/
 int main(void)
+{
 _putchar('_');
 _putchar('p');
 _putchar('u');
