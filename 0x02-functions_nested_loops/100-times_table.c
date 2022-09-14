fd32bec872bbde6d6c783ff12089ce6bb6265309
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-14 01:02:33 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,8 @@ for (num = 0; num <= n; num++)
 _putchar('0');
 for (mult = 1; mult <= n; mult++)
 {
-_putchar(',');									_putchar(' ');
+_putchar(',');
+_putchar(' ');
 prod = num * mult;
 if (prod <= 99)
 _putchar(' ');
