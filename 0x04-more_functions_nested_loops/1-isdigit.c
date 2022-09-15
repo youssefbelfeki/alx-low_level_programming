67e5953c508bc812443adf620a6166053cf19d5b
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-15 12:11:58 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _isdigit - checks for characters that are digits
- * @c: character 
- * Return: 0 or 1
+ * _isdigit - checks if a character is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
  */
 int _isdigit(int c)
 {
-if ((c >= 48) && (c <= 57))
-{
+if (c >= '0' && c <= '9')
 return (1);
-}
+else
 return (0);
 }
