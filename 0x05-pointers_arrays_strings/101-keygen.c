17d0f6c78469c509b6232d7f15b77c0fe5f4ed1f
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-19 02:46:04 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,24 @@
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Program that generates random valid passwords
+ * @void: Empty input
+ * Description: Program that generates random valid passwords
+ * Return: 0 if works
  */
 int main(void)
 {
+int i = 0, random, checksum;
+srand(time(NULL));
+checksum = 2772;
+while (checksum > 122)
+{
+random = (rand() % 100);
+printf("%c", random);
+checksum -= random;
+i++;
+}
+printf("%c", checksum);
 return (0);
 }
