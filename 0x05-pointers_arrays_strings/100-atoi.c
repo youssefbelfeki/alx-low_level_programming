2e4bb0eced53237ac1ba4cfe867de5d10ecb3998
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-19 02:25:16 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,30 @@
 #include "main.h"
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - convert string to int
+ * @s: string
+ * Return: Number
  */
 int _atoi(char *s)
 {
+int i = 0;
+int res;
+while (s[i] != '\0')
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+if (s[i - 1] == '-')
+{
+_putchar('-');
+}
+if ((s[i + 1] > '9') || (s[i + 1] < '0'))
+{
+_putchar(s[i]);
+break;
+}
+res = s[i];
+_putchar(res);
+}
+i++;
+}
 return (0);
 }
