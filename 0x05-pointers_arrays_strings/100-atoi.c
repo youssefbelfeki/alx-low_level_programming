d14a745528a6d784d326b3c5d5e0f592b403c7d0
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-19 02:40:52 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,25 +6,25 @@
  */
 int _atoi(char *s)
 {
+int counter = 0;/* A */
 int i = 0;
-int res;
-while (s[i] != '\0')
+unsigned int result = 0;
+int sign = 0;
+while (s[counter] != '\0')/* B */
 {
-if (s[i] >= '0' && s[i] <= '9')
+if (s[counter] == '-')/* C */
+sign++;
+if (s[counter] >= 48 && s[counter] <= 57)/* D */
 {
-if (s[i - 1] == '-')
-{
-_putchar('-');
-}
-if ((s[i + 1] > '9') || (s[i + 1] < '0'))
-{
-_putchar(s[i]);
-break;
-}
-res = s[i];
-_putchar(res);
-}
 i++;
+result = result * 10 + s[counter] - '0';/* E */
+}										if (i > 0 && (s[counter] < 48 || s[counter] > 57))/* F */
+break;
+counter++;/* G */
 }
+if (i == 0)/* H */
 return (0);
+if (sign % 2 == 0)/* I */
+return (result);
+return (result * -1);/* J */
 }
