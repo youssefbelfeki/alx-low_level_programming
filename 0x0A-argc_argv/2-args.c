75939f6d3e7ed659220f58631e56385db846b538
youssefbelfeki <youssef.belfeki@gmail.com>
2022-09-30 12:22:01 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Prints the number of arguments passed into main
+ * main - Prints all arguments passed into main
  * @argc: Number of command line arguments
  * @argv: Array name
  * Return: 0
@@ -9,13 +9,7 @@
 int main(int argc, char *argv[])
 {
 int i;
-if (argc == 1)
-printf("%d\n", argc - 1);
-else
-{
-for (i = 0; *argv; i++, argv++)
-;
-printf("%d\n", i - 1);
-}
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
 return (0);
 }
