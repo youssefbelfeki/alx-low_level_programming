7e448b44a2c92404540346cdd0caecc57822da4c
youssefbelfeki <youssef.belfeki@gmail.com>
2022-10-06 02:50:14 -0700

    _putchar.c

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,28 +1,11 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include <unistd.h>
 /**
- * main - check the code for ALX School students.
- * Return: Always 0.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-char *c;
-int *i;
-float *f;
-double *d;
-c = malloc_checked(sizeof(char) * 1024);
-printf("%p\n", (void *)c);
-i = malloc_checked(sizeof(int) * 402);
-printf("%p\n", (void *)i);
-f = malloc_checked(sizeof(float) * 100000000);
-printf("%p\n", (void *)f);
-d = malloc_checked(INT_MAX);
-printf("%p\n", (void *)d);
-free(c);
-free(i);
-free(f);
-free(d);
-return (0);
+return (write(1, &c, 1));
 }
