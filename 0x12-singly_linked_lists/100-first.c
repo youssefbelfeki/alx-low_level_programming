ab5feb3bb3c0eeff62379c26395b6a408d18fcac
youssefbelfeki <youssef.belfeki@gmail.com>
2022-10-21 00:23:40 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,17 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
- * reverse_listint - Reverses a listint_t list.
- * @head: A pointer to the address of
- * the head of the list_t list.
- * Return: A pointer to the first node of the reversed list.
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
  */
-listint_t *reverse_listint(listint_t **head)
+void free_list(list_t *head)
 {
-listint_t *ahead, *behind;
-if (head == NULL || *head == NULL)
-return (NULL);
-behind = NULL;
-while ((*head)->next != NULL)
+list_t *tmp;
+while (head)
 {
-ahead = (*head)->next;
-(*head)->next = behind;
-behind = *head;
-*head = ahead;
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
 }
-(*head)->next = behind;
-return (*head);
 }
